69999c7628a9344907f3154ae89b7da18d5d187d
rashmikathakur <thimu.15406@gmail.com>
2026-09-02 12:02:24 +0530

    leetcode problem 35

--- exp1.1/238.cpp
+++ exp1.1/238.cpp
@@ -1,20 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> answer(n, 1);
 
-        // Store product of all elements to the left
         int prefix = 1;
 
+        // Product of elements to the left
         for (int i = 0; i < n; i++) {
             answer[i] = prefix;
             prefix *= nums[i];
         }
 
-        // Multiply by product of all elements to the right
         int suffix = 1;
 
+        // Product of elements to the right
         for (int i = n - 1; i >= 0; i--) {
             answer[i] *= suffix;
             suffix *= nums[i];
